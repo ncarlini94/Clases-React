b92cdccb4118dd66ddd4efd1632e7ad512badf22
ncarlini94 <ncarlini94@gmail.com>
2022-10-06 15:46:32 -0300

    Clase08

--- Clases/Clase08/js/app.js
+++ Clases/Clase08/js/app.js
@@ -1,9 +1,8 @@
 'use strict'; //
-import Button from "./components/Button.js";
-import Video from "./components/Video.js";
+import Video from "./js/Video";
+import Button from "./js/Button";
 
 const ReactElement = React.createElement;
-
 
 const domContainer = document.getElementById("myApp");
 ReactDOM.render([ReactElement(Button), ReactElement(Video)] , domContainer);
