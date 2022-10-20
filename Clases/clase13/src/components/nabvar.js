bdca344d13afd2ebc4aed4dbb86bd4f0ace71ee8
ncarlini94 <ncarlini94@gmail.com>
2022-10-20 18:02:03 -0300

    Clase13 practica Context

---
+++ Clases/clase13/src/components/nabvar.js
@@ -0,0 +1,15 @@
+import React , { Components } from 'react';
+
+export default class Navbar extends Components {
+    constructor(){
+        console.log("contruyendo el Navbar..")
+    }
+
+    render(){
+        return(
+            <>
+        
+            </>
+        )
+    }
+}
